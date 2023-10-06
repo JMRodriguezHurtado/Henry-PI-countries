3b5f4a7b9fd2f5a50a2cf64814261a0f84c8f522
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-10-05 22:15:11 -0600

    correction

--- cr-pi-countries-main/server/src/controllers/getCountries.js
+++ cr-pi-countries-main/server/src/controllers/getCountries.js
@@ -1,52 +1,15 @@
-const axios = require('axios')
-const { Country } = require('../db')
-const db = require('../db')
+const axios = require("axios");
+const Sequelize = require('sequelize');
+const {Country, Activity} = require("../db")
 
-const URL = `http://localhost:5000/countries`
+  const getCountries = async () => {
+    const countries = await Country.findAll({
+      include: {
+        model: Activity,
+        through: { attributes: [] },
+      },
+    });
+    return countries;
+  };
 
-
-const getCountries = async() => {
-    const response = await axios(URL)
-
-    const allCountries = response.data
-
-    let dbCountries = []
-
-    for (const country of allCountries) {
-
-        if(!country.capital){
-            country.capital = ['-']
-        }
-        if(!country.subregion){
-            country.subregion = '-'
-        }
-
-        await Country.findOrCreate({
-            where: {
-                id: country.cca3,
-                name: country.name.official,
-                image: country.flags.png,
-                continent: country.region,
-                capital: country.capital[0],
-                subregion: country.subregion,
-                area: country.area,
-                population: country.population
-            }
-        });
-
-        dbCountries.push({
-            id: country.cca3,
-            name: country.name.official,
-            image: country.flags.png,
-            continent: country.region,
-            capital: country.capital[0],
-            subregion: country.subregion,
-            area: country.area,
-            population: country.population
-        })
-    }
-
-    return dbCountries
-}
-
-module.exports = getCountries
+module.exports = getCountries;
